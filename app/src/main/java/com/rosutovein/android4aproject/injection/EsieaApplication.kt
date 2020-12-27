dc1faecf2861a0b9b401694dba1dd8cb36bc8068
Alex <piga@esiea.fr>
2020-12-27 23:28:59 +0100

    Add an icon launcher and a SplashScreen

--- app/src/main/java/com/rosutovein/android4aproject/injection/EsieaApplication.kt
+++ app/src/main/java/com/rosutovein/android4aproject/injection/EsieaApplication.kt
@@ -1,12 +1,14 @@
 package com.rosutovein.android4aproject.injection
 
 import android.app.Application
+import android.os.SystemClock
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
 class EsieaApplication : Application() {
     override fun onCreate(){
         super.onCreate()
+        SystemClock.sleep(2000)
         // start Koin!
         startKoin {
             androidContext(this@EsieaApplication)
