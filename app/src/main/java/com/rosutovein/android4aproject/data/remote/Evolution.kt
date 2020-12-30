1fcdcaa7638a1f1f7c21f698d9b1158092b307de
Alex <piga@esiea.fr>
2020-12-30 01:02:31 +0100

    Idea changement (from weather to PokeAPIV2)

---
+++ app/src/main/java/com/rosutovein/android4aproject/data/remote/Evolution.kt
@@ -0,0 +1,7 @@
+package com.rosutovein.android4aproject.data.remote
+
+class Evolution {
+    var num : String? = null
+    var name : String? = null
+
+}
---
+++ app/src/main/java/com/rosutovein/android4aproject/data/remote/Pokedex.kt
@@ -0,0 +1,6 @@
+package com.rosutovein.android4aproject.data.remote
+
+class Pokedex {
+
+    var pokemon: List<Pokemon>? = null
+}
